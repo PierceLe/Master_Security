eef6369adcde1d45f573dba8a9b648e209cd9568
PierceLe <haichau.hvsg@gmail.com>
2025-02-09 01:58:37 +0700

    build: building the scaffold of the project

--- A2/app.py
+++ A2/app.py
@@ -1,14 +1,16 @@
-'''
-app.py contains all of the server application
-this is where you'll find all of the get/post request handlers
+"""
+app.py contains all the server application
+this is where you'll find all the get/post request handlers
 the socket event handlers are inside of socket_routes.py
-'''
+"""
 
 from flask import Flask, render_template, request, abort, url_for
 from flask_socketio import SocketIO
 import db
 import secrets
 
+# don't remove this!!
+import socket_routes
 
 import os
 
@@ -24,18 +26,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex()
 socketio = SocketIO(app)
 
-# don't remove this!!
-import socket_routes
 
 # index page
 @app.route("/")
 def index():
     return render_template("index.jinja")
 
+
 # login page
 @app.route("/login")
-def login():    
+def login():
     return render_template("login.jinja")
+
 
 # handles a post request when the user clicks the log in button
 @app.route("/login/user", methods=["POST"])
@@ -54,10 +56,13 @@ def login_user():
         return "Error: Password does not match!"
 
     return url_for('home', username=request.json.get("username"))
+
+
 # handles a get request to the signup page
 @app.route("/signup")
 def signup():
     return render_template("signup.jinja")
+
 
 # handles a post request when the user clicks the signup button
 @app.route("/signup/user", methods=["POST"])
@@ -72,10 +77,12 @@ def signup_user():
         return url_for('home', username=username)
     return "Error: User already exists!"
 
+
 # handler when a "404" error happens
 @app.errorhandler(404)
 def page_not_found(_):
     return render_template('404.jinja'), 404
+
 
 # home page, where the messaging app is
 @app.route("/home")
@@ -83,7 +90,6 @@ def home():
     if request.args.get("username") is None:
         abort(404)
     return render_template("home.jinja", username=request.args.get("username"))
-
 
 
 if __name__ == '__main__':
