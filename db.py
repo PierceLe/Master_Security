9a8cd0612f12bf7e3112a669a3482ffa6303259c
PierceLe <haichau.hvsg@gmail.com>
2025-02-13 22:13:37 +0700

    add: design db and creating table using sqlalchemy

--- db.py
+++ db.py
@@ -20,15 +20,3 @@ engine = create_engine("sqlite:///database/main.db", echo=False)
 
 # initializes the database
 Base.metadata.create_all(engine)
-
-# inserts a user to the database
-def insert_user(username: str, password: str):
-    with Session(engine) as session:
-        user = User(username=username, password=password)
-        session.add(user)
-        session.commit()
-
-# gets a user from the database
-def get_user(username: str):
-    with Session(engine) as session:
-        return session.get(User, username)
--- models.py
+++ models.py
@@ -3,16 +3,16 @@ models
 defines sql alchemy data models
 also contains the definition for the room class used to keep track of socket.io rooms
 
-Just a sidenote, using SQLAlchemy is a pain. If you want to go above and beyond, 
-do this whole project in Node.js + Express and use Prisma instead, 
+Just a sidenote, using SQLAlchemy is a pain. If you want to go above and beyond,
+do this whole project in Node.js + Express and use Prisma instead,
 Prisma docs also looks so much better in comparison
 
 or use SQLite, if you're not into fancy ORMs (but be mindful of Injection attacks :) )
 '''
 
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from typing import Dict
+from sqlalchemy import ForeignKey, Integer, String, TIME, DateTime, Boolean, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List
 
 
 # data models
@@ -20,54 +20,83 @@ class Base(DeclarativeBase):
     pass
 
 
-# model to store user information
+class Relationships(Base):
+    __tablename__ = 'relationships'
+    relationship_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('users.user_id'))
+    friend_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('users.user_id'))
+    status: Mapped[Integer] = mapped_column(Integer)
+
+
 class User(Base):
-    __tablename__ = "user"
-
-    # looks complicated but basically means
-    # I want a username column of type string,
-    # and I want this column to be my primary key
-    # then accessing john.username -> will give me some data of type string
-    # in other words we've mapped the username Python object property to an SQL column of type String 
-    username: Mapped[str] = mapped_column(String, primary_key=True)
+    __tablename__ = "users"
+    user_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    fullname: Mapped[str] = mapped_column(String)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    avatar: Mapped[str] = mapped_column(String)
+    pb_key: Mapped[str] = mapped_column(String)
     password: Mapped[str] = mapped_column(String)
+    salt: Mapped[str] = mapped_column(String)
+    role: Mapped[str] = mapped_column(String)
+    online: Mapped[bool] = mapped_column(Boolean, default=False)
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+    relationships: Mapped[List["Relationships"]] = relationship(foreign_keys="[Relationships.user_id]")
 
 
-# stateful counter used to generate the room id
-class Counter():
-    def __init__(self):
-        self.counter = 0
+class Message(Base):
+    __tablename__ = "messages"
+    message_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    kind: Mapped[str] = mapped_column(String, default="text")
+    content: Mapped[str] = mapped_column(String)
+    author: Mapped[int] = mapped_column(Integer, ForeignKey('users.user_id'))
+    room_id: Mapped[int] = mapped_column(Integer, ForeignKey('rooms.room_id'))
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
-    def get(self):
-        self.counter += 1
-        return self.counter
+
+class Comment(Base):
+    __tablename__ = "comments"
+    comment_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    article_id: Mapped[str] = mapped_column(Integer, ForeignKey('articles.article_id'))
+    content: Mapped[str] = mapped_column(String)
+    author: Mapped[str] = mapped_column(Integer, ForeignKey('users.user_id'))
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
 
-# Room class, used to keep track of which username is in which room
-class Room():
-    def __init__(self):
-        self.counter = Counter()
-        # dictionary that maps the username to the room id
-        # for example self.dict["John"] -> gives you the room id of 
-        # the room where John is in
-        self.dict: Dict[str, int] = {}
+class Article(Base):
+    __tablename__ = "articles"
+    article_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    content: Mapped[str] = mapped_column(String)
+    author: Mapped[int] = mapped_column(Integer, ForeignKey('users.user_id'))
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
-    def create_room(self, sender: str, receiver: str) -> int:
-        room_id = self.counter.get()
-        self.dict[sender] = room_id
-        self.dict[receiver] = room_id
-        return room_id
 
-    def join_room(self, sender: str, room_id: int) -> int:
-        self.dict[sender] = room_id
+class UserRoom(Base):
+    __tablename__ = "user_rooms"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[str] = mapped_column(Integer, ForeignKey('users.user_id'))
+    room_id: Mapped[str] = mapped_column(Integer, ForeignKey('rooms.room_id'))
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    def leave_room(self, user):
-        if user not in self.dict.keys():
-            return
-        del self.dict[user]
 
-    # gets the room id from a user
-    def get_room_id(self, user: str):
-        if user not in self.dict.keys():
-            return None
-        return self.dict[user]
+class Room(Base):
+    __tablename__ = "rooms"
+    room_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    room_name: Mapped[str] = mapped_column(String)
+    room_img: Mapped[str] = mapped_column(String)
+    room_type: Mapped[str] = mapped_column(String, default="single")
+    room_master: Mapped[int] = mapped_column(Integer, ForeignKey('users.user_id'), nullable=True)
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+
+
+class Block(Base):
+    __tablename__ = "blocks"
+    block_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    type_block: Mapped[str] = mapped_column(String)
+    user_block: Mapped[str] = mapped_column(Integer, ForeignKey('users.user_id'))
+    time_created: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now())
+    time_updated: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
